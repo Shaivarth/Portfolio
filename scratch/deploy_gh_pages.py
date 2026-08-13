c77edcb23e8fd2a1e99453f5f6ab0071bc6b1dca
shaivarth <sarthakmishra080803@gmail.com>
2026-08-13 20:04:53 +0530

    Update deploy script

--- scratch/deploy_gh_pages.py
+++ scratch/deploy_gh_pages.py
@@ -6,6 +6,11 @@ repo_dir = r"c:\Users\sarth\Downloads\shaivarth-portfolio"
 public_dir = os.path.join(repo_dir, "public")
 temp_dir = os.path.join(repo_dir, ".gh-pages-deploy")
 
+def remove_readonly(func, path, exc_info):
+    import stat
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
 # Run clean production build
 print("Building production site with Hugo...")
 subprocess.run(["hugo", "--gc", "--minify", "-e", "production"], cwd=repo_dir, check=True)
@@ -23,11 +28,6 @@ subprocess.run(["git", "add", "."], check=True)
 subprocess.run(["git", "commit", "-m", "Deploy complete site to gh-pages"], check=True)
 subprocess.run(["git", "remote", "add", "origin", "https://github.com/Shaivarth/Portfolio.git"], check=True)
 subprocess.run(["git", "push", "-f", "origin", "gh-pages"], check=True)
-
-def remove_readonly(func, path, exc_info):
-    import stat
-    os.chmod(path, stat.S_IWRITE)
-    func(path)
 
 os.chdir(repo_dir)
 if os.path.exists(temp_dir):
